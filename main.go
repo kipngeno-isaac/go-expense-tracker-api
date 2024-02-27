aa324c539eb1f9aa55959c3b76b4bd0611c5a9e3
Isaac Kipngeno <isaackipngeno@HP-250-15-6-inch-G9-Notebook-PC-72532965.localdomain>
2024-02-27 12:42:04 +0300

    add sign up route

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/kipngeno-isaac/go-expense-tracker-api/config"
+	"github.com/kipngeno-isaac/go-expense-tracker-api/controllers"
 )
 
 func init() {
@@ -19,5 +20,6 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.POST("sign-up", controllers.SignUp)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
