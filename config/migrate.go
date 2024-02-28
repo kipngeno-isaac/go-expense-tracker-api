8c699189cb9380c1a1a72b0b969b6253fa0ce5f7
Isaac Kipngeno <isaackipngeno@HP-250-15-6-inch-G9-Notebook-PC-72532965.localdomain>
2024-02-28 11:20:56 +0300

    add category model and migrate

--- config/migrate.go
+++ config/migrate.go
@@ -3,5 +3,5 @@ package config
 import "github.com/kipngeno-isaac/go-expense-tracker-api/models"
 
 func Migrate() {
-	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.User{}, &models.Category{})
 }
